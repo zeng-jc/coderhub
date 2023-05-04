cee6d62a4fb19f6865e2f16aaa738787984ca68c
zengjiachengcode <2356924146@qq.com>
2023-05-04 14:37:13 +0800

    ✨ feat(配置文件): 注释跨域代理的配置代码

--- src/service/request/config.js
+++ src/service/request/config.js
@@ -1,2 +1,3 @@
+// export const BASE_URL = '/api' // 代理接口
 export const BASE_URL = 'http://localhost:8000'
 export const TIMEOUT = 8000
--- vite.config.js
+++ vite.config.js
@@ -26,4 +26,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:8000',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // }
 })

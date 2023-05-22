224f8cb291925f093e8af02f32eca1cded5f1df7
zengjiachengcode <2356924146@qq.com>
2023-05-22 23:49:54 +0800

    ✨ feat(router): 路由跳转后通过前置导航守卫修改网页title

--- src/router/index.js
+++ src/router/index.js
@@ -10,33 +10,50 @@ const router = createRouter({
     {
       path: '/home',
       name: 'home',
-      component: () => import('../views/home/home-view.vue')
+      component: () => import('../views/home/home-view.vue'),
+      meta: {
+        title: '首页'
+      }
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/login/login-view.vue')
+      component: () => import('../views/login/login-view.vue'),
+      meta: {
+        title: '登录'
+      }
     },
     {
       path: '/signup',
       name: 'signup',
-      component: () => import('../views/signup/signup-view.vue')
+      component: () => import('../views/signup/signup-view.vue'),
+      meta: {
+        title: '注册'
+      }
     },
     {
       path: '/detail/:id',
       name: 'detail',
-      component: () => import('../views/detail/detail-view.vue')
+      component: () => import('../views/detail/detail-view.vue'),
+      meta: {
+        title: '详情'
+      }
     },
     {
       path: '/user/:username',
       name: 'user',
-      component: () => import('../views/user/user-view.vue')
+      component: () => import('../views/user/user-view.vue'),
+      meta: {
+        title: '用户主页'
+      }
     }
   ]
 })
 
-// router.beforeEach((to) => {
-//   if (to.name === 'user' && !localStorage.getItem('token')) return router.replace('/login')
-// })
+router.beforeEach((to) => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+})
 
 export default router
